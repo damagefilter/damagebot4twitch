a721074827c1ef179ea77bece64539fb375ddf4e
Chris Ksoll <chris@playblack.net>
2017-06-10 14:08:53 +0200

    Add IsBroadcaster property to ChatStatus
    It's a common thing to ask for so there goes.
    Basically just checks the elevation level.

--- DamageLib/Users/ChatStatus.cs
+++ DamageLib/Users/ChatStatus.cs
@@ -3,6 +3,8 @@ namespace DamageBot.Users {
     /// Contains information about the last messages chat status.
     /// Since it is sent through twitch on every message it would appear
     /// to make no sense to store it anywhere.
+    /// Also, while you CAN change the values here it's actually read-only.
+    /// This will be renewed on ever message.
     /// </summary>
     public struct ChatStatus {
         
@@ -22,6 +24,11 @@ namespace DamageBot.Users {
             get;
             set;
         }
+        
+        /// <summary>
+        /// Is this the channel operator (broadcaster)?
+        /// </summary>
+        public bool IsBroadcaster => this.UserElevation == Elevation.Broadcaster;
 
         public string Channel {
             get;
